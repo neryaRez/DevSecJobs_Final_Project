2c7eb84be40b9554f93e8e907da09062cf2b51a9
neryaRez <neryarez@gmail.com>
2026-01-08 01:21:39 +0200

    k8s4

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./output.css";
+import "./index.css";
 import { AuthProvider } from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
