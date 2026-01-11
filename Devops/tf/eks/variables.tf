cdc561e3276d3437571f22e1f906105ac37d4096
neryaRez <neryarez@gmail.com>
2026-01-11 19:33:28 +0200

    tf1

---
+++ Devops/tf/eks/variables.tf
@@ -0,0 +1,20 @@
+variable "aws_region" {
+    description = "The AWS region to deploy resources in"
+    type        = string
+    default     = "us-east-1"
+}
+variable "cluster_name" {
+    description = "The name of the EKS cluster"
+    type        = string
+    default     = "devsecjobs-eks"
+}
+variable "k8s_version" {
+    description = "The Kubernetes version for the EKS cluster"
+    type        = string
+    default     = "1.29"
+}
+variable "node_instance_type" {
+    description = "The instance type for the EKS worker nodes"
+    type        = string
+    default     = "t3.medium"
+}
